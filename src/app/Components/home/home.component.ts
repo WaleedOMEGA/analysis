bc46808703732777c598190b6f8f7b34a3c36da6
WALEEDSALAH-PC\Professional <waleedomega@hotmail.com>
2022-05-24 15:18:09 +0200

    adding chart

---
+++ src/app/Components/home/home.component.ts
@@ -0,0 +1,153 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { DataService } from './../../Services/data.service';
+import { DataModel } from './../../Models/data.model';
+import * as Highcharts from 'highcharts';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  ListOfCountries: any[] = [];
+  allData = [];
+  ListOfCamp: any[] = [];
+  ListOfSchool: any[] = [];
+  filter: FormGroup = new FormGroup({});
+  loading: boolean = true;
+  filteredData = [];
+  Highcharts: typeof Highcharts = Highcharts;
+
+  chartOptions: Highcharts.Options = {
+    chart: {
+      renderTo: 'container',
+      marginLeft: 100,
+      //  plotAreaWidth: 50,
+      //   plotAreaHeight: 450,
+    },
+
+    title: {
+      text: 'Bar series - data sorting',
+    },
+
+    yAxis: {
+      title: {
+        text: '',
+      },
+    },
+
+    xAxis: {
+      type: 'category',
+      min: 0,
+      labels: {
+        // animate: false
+      },
+    },
+
+    legend: {
+      enabled: false,
+    },
+
+    series: [
+      {
+        type: 'line',
+        zoneAxis: 'x',
+        zones: [
+          {
+            value: 2,
+            color: 'red',
+          },
+        ],
+        dataLabels: {
+          enabled: true,
+          format: '{y:,.2f}',
+        },
+        data: [
+          ['hello', 1],
+          ['heddllo', 5],
+          ['heldsalo', 2],
+          ['heldsadsalo', 4],
+        ],
+      },
+    ],
+  };
+  constructor(private data: DataService, private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.getAllData();
+    this.filter = this.fb.group({
+      country: [''],
+      camp: [''],
+      school: [''],
+    });
+
+  }
+  getAllData() {
+    this.data.getData().subscribe({
+      next: this.handelResponse.bind(this),
+    });
+  }
+  handelResponse(response: any) {
+    this.allData = response;
+    console.log(this.allData);
+    this.ListOfCountries = [
+      ...new Set(this.allData.map((item: DataModel) => item.country)),
+    ];
+    console.log(this.ListOfCountries);
+    this.ListOfCamp = [
+      ...new Set(this.allData.map((item: DataModel) => item.camp)),
+    ];
+    console.log(this.ListOfCamp);
+    this.ListOfSchool = [
+      ...new Set(this.allData.map((item: DataModel) => item.school)),
+    ];
+    console.log(this.ListOfSchool);
+    this.loading = false;
+  }
+  changeFilter() {
+    this.getFilteredData(this.filter.value);
+    this.initChart();
+  }
+  getFilteredData(value: any) {
+    this.filteredData = this.allData;
+    if (value.country) {
+      this.filteredData = this.filteredData.filter(
+        (item: DataModel) => item.country === value.country
+      );
+    }
+    if (value.camp) {
+      this.filteredData = this.filteredData.filter(
+        (item: DataModel) => item.camp === value.camp
+      );
+    }
+    if (value.school) {
+      this.filteredData = this.filteredData.filter(
+        (item: DataModel) => item.school === value.school
+      );
+    }
+    console.log(this.filteredData);
+  }
+  public initChart() {
+      Highcharts.chart('container', this.chartOptions);
+    // let chart = new Highcharts.Chart({
+    //   chart: {
+    //     type: 'line',
+    //   },
+    //   title: {
+    //     text: 'Linechart',
+    //   },
+    //   credits: {
+    //     enabled: false,
+    //   },
+    //   series: [
+    //     {
+    //       name: 'Line 1',
+    //       data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    //     },
+    //   ],
+    // });
+
+    // this.chart = chart;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './Components/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ChartModule } from 'angular-highcharts';
+import { HighchartsChartModule } from 'highcharts-angular';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
+    ReactiveFormsModule,
+    ChartModule,
+    HighchartsChartModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
